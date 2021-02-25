32ff3932aff509f1f91b03e530c2dab2d57ec41e
Liviu Ionescu <ilg@livius.net>
2021-02-25 16:45:08 +0200

    cmake: simplify, keep only config.cmake

--- meta/micro-os-plus-libs-cpp-config.cmake
+++ meta/micro-os-plus-libs-cpp-config.cmake
@@ -12,11 +12,60 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including micro-os-plus-libs-cpp-config...")
+if(micro-os-plus-libs-cpp-included)
+  return()
+endif()
+
+set(micro-os-plus-libs-cpp-included TRUE)
+
+message(STATUS "Including micro-os-plus-libs-cpp...")
+
+# -----------------------------------------------------------------------------
+# Dependencies.
+
+find_package(micro-os-plus-diag-trace REQUIRED)
+
+# -----------------------------------------------------------------------------
+# The current folder.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+if(NOT TARGET micro-os-plus-libs-cpp-interface)
+
+  add_library(micro-os-plus-libs-cpp-interface INTERFACE EXCLUDE_FROM_ALL)
+
+  # ---------------------------------------------------------------------------
+
+  target_sources(
+    micro-os-plus-libs-cpp-interface
+
+    INTERFACE
+      ${xpack_current_folder}/src/cxx.cpp
+      ${xpack_current_folder}/src/system-error.cpp
+  )
+
+  target_include_directories(
+    micro-os-plus-libs-cpp-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  target_link_libraries(
+    micro-os-plus-libs-cpp-interface
+    
+    INTERFACE
+      micro-os-plus::diag-trace-static
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::libs-cpp ALIAS micro-os-plus-libs-cpp-interface)
+  message(STATUS "micro-os-plus::libs-cpp")
+
+endif()
 
 # -----------------------------------------------------------------------------
-
